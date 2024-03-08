90e298b3b3984dc72dfe581262f417214d5125d4
s.navidi <shnavidi18@gmail.com>
2024-03-08 18:58:29 +0330

    add package (axios,react-query,tailwind),fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css';
-
-function App() {
-  return <>گلرنگ سیستم</>;
-}
+const App = () => {
+  return (
+    <div className="container text-4xl xl:max-w-screen-xl">گلرنگ سیستم</div>
+  );
+};
 
 export default App;
